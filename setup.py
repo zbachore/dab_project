d930f467771e709a8fcee6b07d2f53753c307ecc
zbelachew <zbelachew@gardenranges.com>
2025-12-23 14:42:27 -0500

    Added Test and Prod SP Configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.2",
+    version="0.0.3",
     description="This contains the code in the ./src directory for the dab_project project.",
     author="Zewdu Bachore",
     packages=find_packages(where="./src"),
